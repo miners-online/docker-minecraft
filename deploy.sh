eaf73a7344395d52cb3cbd95534af9c47da85882
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-04-29 18:32:21 +0100

    refactor: separate proxy and games environment files

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
-set -a
-. ./.env
-set +a
-
 cd services/proxy
 docker compose up -d
 cd ../../
 
 cd services/games
+set -a
+. ./.env
+set +a
 docker stack deploy --compose-file docker-compose.yml minecraft
 cd ../../
